63f4b228627eabc138809cb3d1b9a02a2528be13
Macarena Baltra <macarena.bc2011@gmail.com>
2017-05-04 09:53:25 -0300

    Subiendo los ultimos cambios antes de Blanca

--- asetss/javascript.js
+++ asetss/javascript.js
@@ -1,15 +1,30 @@
-function Pokemon(nombre,color){
+function Pokemon(nombre,color,poderDeAtaque){
     this.nombre = nombre;
     this.color = color;
 
     this.nivelDeAmistad = 0;
 
+    this.vida = 100;
+    this.poderDeAtaque = poderDeAtaque;
+
     this.mostrarPokemon = function(){
-      return('Hola, soy: ' + this.nombre + ' y soy de color: ' + this.color)
+      return('Hola, soy: ' + this.nombre + ' y soy de color: ' + this.color);
     }
 
     this.aumentarAmistad = function(valor){
       this.nivelDeAmistad = this.nivelDeAmistad + valor;
     }
+
+    this.atacar = function(pokemon){
+      pokemon.vida = pokemon.vida - this.poderDeAtaque;
+    }
 }
-const Pikachu = new Pokemon('Pikachu','amarillo');
+const Pikachu = new Pokemon('Pikachu','amarillo',100);
+const Charmander = new Pokemon('Charmander','rojo',300);
+const Jigglypuff = new Pokemon('Jigglypuff','rosado',50);
+const Leafeon = new Pokemon('Leafeon','crudo con verde',300);
+const Cubone = new Pokemon('Cubone','cafe',700);
+const Chanchicornio = new Pokemon('Chanchicornio','blanco con rosado',700);
+
+Pikachu.atacar(Charmander);
+console.log(Charmander.vida);
